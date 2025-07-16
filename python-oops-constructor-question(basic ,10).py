cc1c73163aa56ba50e81cde8b9f9e295a04a68f6
parul pal <parulpal7088@gmail.com>
2025-07-16 12:26:00 +0530

    Update python-oops-constructor-question(basic ,10).py

--- python-oops-constructor-question(basic ,10).py
+++ python-oops-constructor-question(basic ,10).py
@@ -75,17 +75,91 @@ class Employee():
 employee_1 = Employee("rahul","5876287683","22,d ox flat")
 employee_1.add_record()
 
-#✅ 5. Class with constructor that converts Celsius to Fahrenheit
-class Temperature:
-    def __init__(self, celsius):
-        self.celsius = celsius
+#✅ 5. Sum of two numbers using constructor
+class add():
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+    def sum(self):
+        print(f"the sum of numbers : {self.num1 + self.num2}")
 
-    def to_fahrenheit(self):
-        f = (self.celsius * 9/5) + 32
-        print(f"{self.celsius}°C = {f}°F")
+number = add(34,89)
+number_1 = add(56,45)
+number.sum()
+number_1.sum()
 
-t = Temperature(37)
-t.to_fahrenheit()
+#✅ 6.Class with constructor that converts Celsius to Fahrenheit
+class temp_convertor():
+    def __init__(self,c):
+        self.c = c
+    def celsius_to_fahrenheit(self):
+        f = (self.c *1.8)+32
+        print("temp in fahrenheit:",f)
+temp_1 = temp_convertor(57)
+temp_2 = temp_convertor(45)
+temp_1.celsius_to_fahrenheit()
+temp_2.celsius_to_fahrenheit()
+
+#✅ 7. Constructor that accepts 3 subject marks and calculates total
+class total_marks():
+    def __init__(self,name,m1,m2,m3):
+        self.name = name
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+    def sum(self):
+        return self.m1+self.m2+self.m3
+student_1 = total_marks("parul",67,78,23)
+student_2 = total_marks("rahul",45,98,100)
+print(f"Total marks of {student_1.name}:",student_1.sum())
+print(f"Total marks of {student_2.name}",student_2.sum())
+
+#✅ 8. Constructor that stores person’s age and checks if eligible to vote
+class To_vote():
+    def __init__(self,age):
+        self.age = age
+    def check_age(self):
+        if self.age >= 18:
+            print("you are eligible to vote ")
+        else:
+            print("you are not eligible to vote ")
+age_1 = To_vote(67)
+age_1.check_age()
+age_2 = To_vote(16)
+age_2.check_age()
+
+#✅ 9. Create a class to reverse a string using constructor
+class Reverser:
+    def __init__(self, text):
+        self.text = text
+
+    def reverse(self):
+        return self.text[::-1]
+
+r = Reverser("Python")
+print("Reversed:", r.reverse())
+
+#✅ 10. Create a class to find the factorial of a number using constructor
+class Factorial():
+    def __init__(self,no):
+        self.no = no
+    def fact(self):
+        if self.no == 1 or 0:
+            print(f"the factorial of {self.no} is 1")
+        else:
+            res = 1
+            for i in range(2, self.no+1):
+                res *= i
+            return res
+no_1 = Factorial(5)
+print(f"the factorial of {no_1.no}:",no_1.fact())
+
+
+
+
+
+
       
 
     
